cd05db0f80d78f853a5f6eedaf5acdf7f024ee82
Mstislav Zhivodkov <stevemyz@gmail.com>
2017-03-29 17:25:26 +0700

    Add dist

---
+++ dist/src/utils.d.ts
@@ -0,0 +1,4 @@
+import { LngLat, Vec2 } from './types';
+export declare function vec2create(): Vec2;
+export declare function lngLatToZoomPoint(out: Vec2, lngLat: LngLat, zoom: number): void;
+export declare const now: any;

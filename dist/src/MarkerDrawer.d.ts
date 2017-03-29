8b20da6a22228ecca1d72d66c43fee9c1e790b78
Mstislav Zhivodkov <stevemyz@gmail.com>
2017-03-29 17:25:26 +0700

    Add dist

---
+++ dist/src/MarkerDrawer.d.ts
@@ -0,0 +1,28 @@
+/// <reference types="leaflet" />
+import { Atlas } from './Atlas';
+import { Marker } from './types';
+export interface MarkerDrawerOptions {
+    debugDrawing?: boolean;
+    bufferFactor?: number;
+    zIndex?: number;
+}
+/**
+ * This class helps to draw fast a huge amount of markers
+ */
+export declare class MarkerDrawer extends L.Layer {
+    private _markers;
+    private _atlas;
+    private _map?;
+    private _renderer;
+    private _pane;
+    constructor(atlas: Atlas, options?: MarkerDrawerOptions);
+    setMarkers(markers: Marker[]): void;
+    update(): void;
+    addTo(map: L.Map): this;
+    onAdd(): this;
+    remove(): this;
+    onRemove(): this;
+    setDebugDrawing(value: boolean): void;
+    private _onClick;
+    private _getMousePosition(ev);
+}
